8b1dc45c726faf0fd125dc1a03339ee8d706f1f7
unknown <chaserichards01@gmail.com>
2020-08-03 10:40:20 -0500

    Completed the basics

--- JavaScript Basics/Challenge 5 - Advanced Tip Calculator.js
+++ JavaScript Basics/Challenge 5 - Advanced Tip Calculator.js
@@ -3,8 +3,83 @@
  * Coding Challenge 5: Advanced Tip Calculator
 */
 
+var tipCalculatorJohn = {
+    billsArray: [124, 48, 268, 180, 42],
+    tipsArray:[],
+    totalBillsArr: [],
+    tipCalculator: function(){
+        for(var i = 0; i < this.billsArray.length; i++){
+            var tipAmount = 0;
+            var totalBillAmount = 0;
+            if(this.billsArray[i] < 50){
+                tipAmount = this.billsArray[i] * .20;
+            } else if(this.billsArray[i] >= 50 && this.billsArray[i] < 200){
+                tipAmount = this.billsArray[i] * .15;
+            } else if(this.billsArray[i] >= 200){
+                tipAmount = this.billsArray[i] * .10;
+            } else{
+                console.log('Invalid bill amount.');
+            }
+            
+            totalBillAmount = this.billsArray[i] + tipAmount;
+            this.tipsArray.push(tipAmount);
+            this.totalBillsArr.push(totalBillAmount);
+        }
+    }
+};
+
+tipCalculatorJohn.tipCalculator();
+console.log(tipCalculatorJohn.tipsArray);
+console.log(tipCalculatorJohn.totalBillsArr);
 
 
 /***************************************************************************
  * Coding Challenge 5 Part 2: Advanced Tip Calculator
 */
+
+var tipCalculatorMark = {
+    billsArray: [77, 375, 110, 45],
+    tipsArray:[],
+    totalBillsArr: [],
+    tipCalculator: function(){
+        for(var i = 0; i < this.billsArray.length; i++){
+            var tipAmount = 0;
+            var totalBillAmount = 0;
+            if(this.billsArray[i] < 100){
+                tipAmount = this.billsArray[i] * .20;
+            } else if(this.billsArray[i] >= 100 && this.billsArray[i] < 300){
+                tipAmount = this.billsArray[i] * .10;
+            } else if(this.billsArray[i] >= 300){
+                tipAmount = this.billsArray[i] * .25;
+            } else{
+                console.log('Invalid bill amount.');
+            }
+            totalBillAmount = this.billsArray[i] + tipAmount;
+            this.tipsArray.push(tipAmount);
+            this.totalBillsArr.push(totalBillAmount);
+        }
+    }
+};
+
+tipCalculatorMark.tipCalculator();
+console.log(tipCalculatorMark.tipsArray);
+console.log(tipCalculatorMark.totalBillsArr);
+
+function calcAvg(tips){
+    var sum = 0;
+    for(var i = 0; i < tips.length; i++){
+        sum += tips[i];
+    }
+    return sum / tips.length;
+}
+
+var johnAvgTip = calcAvg(tipCalculatorJohn.tipsArray);
+var markAvgTip = calcAvg(tipCalculatorMark.tipsArray);
+
+if(johnAvgTip > markAvgTip){
+    console.log('Johns family paid the highest tips on average.');
+} else if(johnAvgTip < markAvgTip){
+    console.log('Marks family paid the highest tips on average.');
+} else{
+    console.log('The families had the same average tips.');
+}
--- JavaScript Basics/Loops and Iterations.js
+++ JavaScript Basics/Loops and Iterations.js
@@ -1,3 +1,43 @@
 /***************************************************************************
  * Loops and Iteration
 */
+
+//for loop
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+
+var chase = ['Chase', 'Richards', 1995, 'programmer', false];
+
+//console.log(chase[0]);
+for(var i = 0; i < chase.length; i++){
+    console.log(chase[i]);
+}
+
+//while loop
+var i = 0;
+while(i < chase.length){
+    console.log(chase[i]);
+    i++;
+}
+
+//Continue and break statements
+var chase = ['Chase', 'Richards', 1995, 'programmer', false];
+
+//continue
+for(var i = 0; i < chase.length; i++){
+    if(typeof chase[i] !== 'string') continue;
+    console.log(chase[i]);
+}
+
+//break
+for(var i = 0; i < chase.length; i++){
+    if(typeof chase[i] !== 'string') break;
+    console.log(chase[i]);
+}
+
+console.log('===================')
+//Loop through chase array backwards
+for(var i = chase.length - 1; i >= 0; i--){
+    console.log(chase[i]);
+}
