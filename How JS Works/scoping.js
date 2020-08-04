246d5b4e6b4b3adaac3ab154ab77f9389dba3fce
unknown <chaserichards01@gmail.com>
2020-08-04 13:28:45 -0500

    Set up for how js works behind the scenes content

---
+++ How JS Works/scoping.js
@@ -0,0 +1,11 @@
+///////////////////////////////////////
+// Lecture: Scoping only way to define new scope is a function not loops
+// lexical scoping: a function that is lexically in another function
+//                  gets access to the scope of the outer function
+
+
+
+
+// Example to show the differece between execution stack and scope chain
+// Execution Stack: order in which functions are called
+// Scope Chain: order in which functions are written lexically
---
+++ How JS Works/thisKeyword.js
@@ -0,0 +1,4 @@
+///////////////////////////////////////
+// Lecture: The this keyword
+// Regular function call: the this keyword points at the global object
+// Method Call: the this variable points to the object that is calling the method
