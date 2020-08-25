18cfdf99d5e2f7525185168c8af99f9b8af15f76
unknown <chaserichards01@gmail.com>
2020-08-25 10:09:24 -0500

    Bind, Call, Apply and question coding challenge added.

--- Advanced JavaScript/Bind Call and Apply.js
+++ Advanced JavaScript/Bind Call and Apply.js
@@ -1,2 +1,103 @@
 ///////////////////////////////////////////
 // Lecture: Bind, Call and Apply
+
+
+var chase = {
+    name: 'Chase',
+    age: 25,
+    job: 'teacher',
+    presentation: function(style, timeOfDay){
+        if(style === 'formal'){
+            console.log('Good ' + timeOfDay + ', ladies and gentlemen! I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old.');
+        } else if(style === 'friendly'){
+            console.log('Hey what\'s up? I\'m ' + this.name + ', I\'m a ' + this.job + ' and I\'m ' + this.age + ' years old. Have a nice ' + timeOfDay + '.');
+        }
+    }
+};
+
+chase.presentation('formal', 'morning');
+chase.presentation('friendly', 'night');
+
+var emily = {
+    name: 'Emily',
+    age: 35,
+    job: 'designer'
+};
+
+//Method Borrowing
+chase.presentation.call(emily, 'friendly', 'afternoon');
+
+//Won't work because function doesn't expect an array
+//chase.presentation.apply(emily, ['friendly', 'afternoon']);
+
+//Bind generates a copy of the function to be stored somewhere. Currying: Lets you preset some arguments
+var chaseFriendly = chase.presentation.bind(chase, 'friendly');
+chaseFriendly('midday');
+chaseFriendly('midnight');
+
+var emilyFormal = chase.presentation.bind(emily, 'formal');
+emilyFormal('night');
+
+
+
+
+
+var years = [1995, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for(var i = 0; i < arr.length; i++){
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el){
+    return 2020 - el;
+}
+
+function isFullAge(limit, el){
+    return el >= limit;
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+console.log(ages);
+console.log(fullJapan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Advanced JavaScript/Coding Challenge 7.js
+++ Advanced JavaScript/Coding Challenge 7.js
@@ -1,2 +1,77 @@
 ///////////////////////////
 //Coding Challenge 7
+
+//Function Constructor
+function Question(theQuestion, answers, correctAnswer){
+    this.theQuestion = theQuestion;
+    this.answers = answers;
+    this.correctAnswer = correctAnswer;
+}
+
+//Create a few questions with the constuctor
+var question1 = new Question('What is my name?', ['Chase', 'John', 'David'], 'Chase');
+var question2 = new Question('What is my favorite sport?', ['Baseball', 'Football', 'Tennis', 'Soccer'], 'Tennis');
+var question3 = new Question('What year was I born?', ['1934', '1995', '2002'], '1995');
+
+//Store them inside an array
+var questionArray = [question1, question2, question3];
+
+//Select a random question and log it to console with possible answers
+function randomQuestion(){
+    var randValue = Math.floor(Math.random() * Math.floor(3)); //Random int 0, 1, or 2
+    var randQuestion = questionArray[randValue];
+
+    console.log(randQuestion.theQuestion);
+    for(var i = 0; i < randQuestion.answers.length; i++){
+        console.log((i + 1) + '. ' + randQuestion.answers[i]);
+    }
+
+    var userAnswer = prompt("Please enter your answer (type a number):");
+    if(randQuestion.answers[userAnswer - 1] === randQuestion.correctAnswer){
+        console.log('Correct!');
+    } else{
+        console.log('Incorrect!');
+    }
+}
+
+randomQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
